06a39b14673ae1232770fd77167666d27eef9f50
t544h <arroujaabidi@gmail.com>
2026-04-03 18:41:30 +0100

    notify server

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const admin = require("firebase-admin");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+app.post("/send-notification", async (req, res) => {
+  const { token, title, body } = req.body;
+
+  try {
+    await admin.messaging().send({
+      token: token,
+      notification: {
+        title: title,
+        body: body,
+      },
+    });
+
+    res.send("Notification sent");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
